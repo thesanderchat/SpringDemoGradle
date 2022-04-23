a273091d6ed8e4ea3815b3b53f9d9a5412588abb
thesanderchat <thesanderchat@gmail.com>
2022-04-23 17:04:13 +0300

    program moved to gradle

---
+++ src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository){
+    return args -> {
+      Student vladislav = new Student("Vladislav", "pishenkovladyslav@gmail.com",
+          LocalDate.of(2001, Month.JUNE, 16));
+      Student oleksandr = new Student ("Oleksandr", "sasha.dzuyniak@gmail.com",
+          LocalDate.of(2002, Month.OCTOBER, 18));
+      repository.saveAll(List.of(oleksandr,vladislav));
+    };
+  }
+}
